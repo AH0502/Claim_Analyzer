88215ff6b120f7ad145fba3992549e209c0840f4
Alex Hagopian <hagopianalexander@gmail.com>
2024-11-02 20:44:52 -0400

    Create Claim class.

---
+++ program.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Generic;
+using System.IO;
+
+public class Claim
+{
+    public int claimID {get; set;};
+    public string policyHolderName {get; set;};
+    public 
+}
