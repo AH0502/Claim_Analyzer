59238d7cb1d1c9dc6ddc9fc79a50663d903e0d8f
Alex Hagopian <hagopianalexander@gmail.com>
2024-11-05 21:08:33 -0500

    Fixed errors related to .NETSDK.
    Was having trouble building and running.
    Fixed error in .txt file where there was a missing value.

--- ClaimAnalyzerApp/Program.cs
+++ ClaimAnalyzerApp/Program.cs
@@ -1,2 +1,66 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace ClaimAnalyzerApp
+{
+
+    public class ClaimAnalyzer
+    {   
+        private List<Dictionary<string, object>> claims = new List<Dictionary<string, object>>();
+
+        public void LoadClaimsFromFile(string filePath)
+        {
+            
+
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = sr.ReadLine()) != null)
+                {
+                    string[] parts = line.Split(',');
+                    Dictionary<string, object> claimsData = new Dictionary<string, object>
+                    {
+                        {"ClaimID", parts[0]},
+                        {"Policy Holder Name", parts[1]},
+                        {"Claim Date", parts[2]},
+                        {"Claim Amount", parts[3]},
+                        {"Claim Type", parts[4]},
+                        {"Claim Location", parts[5]}
+                    };
+
+                    claims.Add(claimsData);
+                }
+            }
+               
+
+        }
+        public void DisplayInfo()
+        {
+            foreach (var claim in claims)
+            {
+                Console.WriteLine($"Policy Holder: {claim["Policy Holder Name"]}, "+
+                                  $"ClaimID: {claim["ClaimID"]}, " + 
+                                  $"Date: {claim["Claim Date"]}, " +
+                                  $"Amount: {claim["Claim Amount"]}, " + 
+                                  $"Type: {claim["Claim Type"]}, " + 
+                                  $"Location: {claim["Claim Location"]}"); 
+            }
+        }
+    
+    }
+    public class Program
+    {
+         static void Main(string[] args)
+         {
+            ClaimAnalyzer analyzer = new ClaimAnalyzer();
+
+            string filePath = "/Users/alexanderhagopian/Documents/repos/Claim_Analyzer/ClaimAnalyzerApp/claims.txt";
+
+            analyzer.LoadClaimsFromFile(filePath);
+            analyzer.DisplayInfo();
+         }
+    }
+
+}
+    
