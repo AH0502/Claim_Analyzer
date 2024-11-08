2da6934a204e7a02849d1d8c69a6636d527bc2f8
Alex Hagopian <hagopianalexander@gmail.com>
2024-11-08 12:16:57 -0500

    Remove untracked files.

--- ClaimAnalyzerApp/obj/Debug/net8.0/ClaimAnalyzerApp.AssemblyInfo.cs
+++ ClaimAnalyzerApp/obj/Debug/net8.0/ClaimAnalyzerApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClaimAnalyzerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35822c2c9c16637b4d900d4d2e2dd89bdc023314")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ea67c684ebfdeb57262d3ef6cbd2e96c05af6be")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClaimAnalyzerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClaimAnalyzerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
